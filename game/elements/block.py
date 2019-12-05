f814fa4cc602a6e2d91d01fa97ba3257d19e565e
jieyr3Rui <2930681195@qq.com>
2019-12-05 18:00:43 +0800

    add elements

---
+++ game/elements/block.py
@@ -0,0 +1,7 @@
+# class block
+import pygame
+
+class block(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
---
+++ game/elements/bullet.py
@@ -0,0 +1,10 @@
+# class bullet
+import pygame
+
+class bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+    
+    def move(self):
+        return
+        
