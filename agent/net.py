5d4a33c2f83ff53a1f3abb501263f0fc0ef74609
jieyr3Rui <2930681195@qq.com>
2019-12-12 15:39:23 +0800

    add net

--- agent/net.py
+++ agent/net.py
@@ -1,1 +1,26 @@
 # init net.py
+import torch.nn as nn
+import torch.nn.functional as F
+
+"""
+state: 
+robot_me pos_x, pos_y, angle
+robot_em pos_x, pos_y, angle
+
+action:
+robot1 v_x, v_y, ro, shoot
+"""
+
+class Net(nn.Module):
+    def __init__(self, n_states, n_actions):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n_states, 50)
+        self.fc1.weight.data.normal_(0, 0.1)   # initialization
+        self.out = nn.Linear(50, n_actions)
+        self.out.weight.data.normal_(0, 0.1)   # initialization
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        actions_value = self.out(x)
+        return actions_value
